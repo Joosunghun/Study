cf486eb30e3fb43ffd5e6d83dc58a76eed6e6c6e
BewB <86991902+Joosunghun@users.noreply.github.com>
2021-11-26 15:00:20 +0900

    Upload

--- Programmers/Hash/Hash_2.cpp
+++ Programmers/Hash/Hash_2.cpp
@@ -1,4 +1,4 @@
-// Hash_2 전화번호 목록
+// Hash_2  전화번호 목록
 
 #include <string>
 #include <vector>
@@ -8,7 +8,7 @@ using namespace std;
 bool solution(vector<string> phone_book) {
 	bool answer = true;
 	
-	for (int i = 0; i < phone_book.size(); i++) {
+	for (int i = 0; i < phone_book.size(); i++) {                                      // i와 j가 다르고, i의 접두사에 j가 있으면 false 반환
 		for (int j = 0; j < phone_book.size(); j++) {
 			if (i != j && phone_book[i].find(phone_book[j]) == 0) {
 				answer = false;
@@ -16,5 +16,5 @@ bool solution(vector<string> phone_book) {
 			}
 		}
 	}
-	return answer;
+	return answer;                                                                          // 그렇지 않으면 true 반환
 }
