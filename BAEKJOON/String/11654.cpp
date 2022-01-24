9ee7f8091a005ee682c35dc86957f0f035ecbedd
BewB <86991902+Joosunghun@users.noreply.github.com>
2022-01-24 12:42:11 +0900

    Upload

---
+++ BAEKJOON/String/11654.cpp
@@ -0,0 +1,15 @@
+// 문제 11654 아스키 코드
+// 알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드 값을 출력하는 프로그램
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	char n;
+
+	cin >> n;
+
+	cout << (int)n << '\n';
+
+	return 0;
+}
